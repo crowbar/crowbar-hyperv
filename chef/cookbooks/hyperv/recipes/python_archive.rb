9fb4ebcd2aa25d277687b8324a491fcf46cf9090
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2013-10-11 17:21:22 +0300

    Making sure that the Python Win32 DLLs will not get registered every time the recipe is executed.

--- chef/cookbooks/hyperv/recipes/python_archive.rb
+++ chef/cookbooks/hyperv/recipes/python_archive.rb
@@ -6,6 +6,7 @@ cookbook_file "#{node[:cache_location]}#{node[:python][:archive]}" do
   not_if {::File.exists?(node[:python][:archive])}
 end
 
+#Extract the packaged Python 2.7.5 archive
 windows_batch "unzip_python275" do
   code <<-EOH
 #{node[:sevenzip][:command]} x #{node[:cache_location]}#{node[:python][:archive]} -o#{node[:python][:path]} -r -y
@@ -13,8 +14,14 @@ EOH
   not_if {::File.exists?(node[:python][:installed])}
 end
 
-windows_batch "register_pywin32" do
-  code <<-EOH
+unless node[:python_win32_registered]
+  #Register Python Win32 DLLs
+  windows_batch "register_pywin32" do
+    code <<-EOH
 #{node[:python][:command]} #{node[:python][:scripts]}\\#{node[:python][:pywin32register]} -install
-EOH
+    EOH
+  end
+  node.set[:python_win32_registered] = true
+  node.save
 end
+
