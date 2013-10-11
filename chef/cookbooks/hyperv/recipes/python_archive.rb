c6b327f35464cb5c0f18380e139cf664ffeb671f
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2013-10-11 16:44:52 +0300

    Register PyWin32 DLLs after extracting the archive

--- chef/cookbooks/hyperv/recipes/python_archive.rb
+++ chef/cookbooks/hyperv/recipes/python_archive.rb
@@ -6,9 +6,15 @@ cookbook_file "#{node[:cache_location]}#{node[:python][:archive]}" do
   not_if {::File.exists?(node[:python][:archive])}
 end
 
-windows_batch "unzip_pymysql" do
+windows_batch "unzip_python275" do
   code <<-EOH
 #{node[:sevenzip][:command]} x #{node[:cache_location]}#{node[:python][:archive]} -o#{node[:python][:path]} -r -y
 EOH
   not_if {::File.exists?(node[:python][:installed])}
+end
+
+windows_batch "register_pywin32" do
+  code <<-EOH
+#{node[:python][:command]} #{node[:python][:scripts]}\\#{node[:python][:pywin32register]} -install
+EOH
 end
