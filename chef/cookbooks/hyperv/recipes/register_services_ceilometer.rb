c37011e52fbcbcc26a76287cacc11e637eb1736c
Vincent Untz <vuntz@suse.com>
2016-02-08 13:18:39 +0100

    hyperv: Fix name of ceilometer service
    We don't want to run the notification agent, that's something else.

--- chef/cookbooks/hyperv/recipes/register_services_ceilometer.rb
+++ chef/cookbooks/hyperv/recipes/register_services_ceilometer.rb
@@ -4,14 +4,14 @@ powershell "register_services" do
   code <<-EOH
     if (-not (Get-Service "#{node[:service][:ceilometer][:name]}" -ErrorAction SilentlyContinue))
     {
-      New-Service -name "#{node[:service][:ceilometer][:name]}" -binaryPathName "`"#{node[:openstack][:bin]}\\#{node[:service][:file]}`" ceilometer-compute-agent `"#{node[:openstack][:ceilometer][:binary]}`" --config-file `"#{node[:openstack][:ceilometer][:config]}\\ceilometer.conf`"" -displayName "#{node[:service][:ceilometer][:displayname]}" -description "#{node[:service][:ceilometer][:description]}" -startupType Automatic
+      New-Service -name "#{node[:service][:ceilometer][:name]}" -binaryPathName "`"#{node[:openstack][:bin]}\\#{node[:service][:file]}`" ceilometer-agent-compute `"#{node[:openstack][:ceilometer][:binary]}`" --config-file `"#{node[:openstack][:ceilometer][:config]}\\ceilometer.conf`"" -displayName "#{node[:service][:ceilometer][:displayname]}" -description "#{node[:service][:ceilometer][:description]}" -startupType Automatic
       Start-Service "#{node[:service][:ceilometer][:name]}"
       Set-Service -Name "#{node[:service][:ceilometer][:name]}" -StartupType Automatic
     }
   EOH
 end
 
-service "ceilometer-compute" do
+service "ceilometer-agent-compute" do
   service_name node[:service][:ceilometer][:name]
   action [:enable, :start]
   subscribes :restart, "template[#{node[:openstack][:ceilometer][:config].gsub(/\\/, "/")}/ceilometer.conf]"
